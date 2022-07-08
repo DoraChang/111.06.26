ba77c1b10a5622ad0edc7918e547e71733cdda59
DoraChang <104436657+DoraChang@users.noreply.github.com>
2022-07-08 22:47:51 +0800

    ex1.cpp
    未完成

---
+++ ex1.cpp
@@ -0,0 +1,6 @@
+#http://sites.google.com/a/chromium.org/chromedriver/downloads
+
+from selenium import webdriver
+
+PATH ="C:/Users/張慈恩/Downloads/chromedriver_win32/chromedriver.exe"
+driver = webdriver.Chrome(PATH)
